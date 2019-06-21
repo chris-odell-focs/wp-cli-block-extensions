831c627f6749eedd9e0ab8342b2beaced4fdcb37
chris-odell-focs <chris.odell@foxdellcodesmiths.com>
2019-06-21 07:37:43 +0100

    behat fix

--- bin/install-package-tests.sh
+++ bin/install-package-tests.sh
@@ -3,9 +3,9 @@
 set -ex
 
 install_db() {
-	mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot -h 127.0.0.1
-	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"127.0.0.1" IDENTIFIED BY "password1"' -uroot -h 127.0.0.1
-	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test_scaffold.* TO "wp_cli_test"@"127.0.0.1" IDENTIFIED BY "password1"' -uroot -h 127.0.0.1
+	mysql -e 'CREATE DATABASE IF NOT EXISTS wp_cli_test;' -uroot -pr00t -h 127.0.0.1
+	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test.* TO "wp_cli_test"@"127.0.0.1" IDENTIFIED BY "password1"' -uroot -pr00t -h 127.0.0.1
+	mysql -e 'GRANT ALL PRIVILEGES ON wp_cli_test_scaffold.* TO "wp_cli_test"@"127.0.0.1" IDENTIFIED BY "password1"' -uroot -pr00t -h 127.0.0.1
 }
 
 install_db
